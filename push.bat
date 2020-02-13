8ee6674209d38ca2845267474f9c6be4e11292b5
ChaosGT <TangGuangchao@gmail.com>
2020-02-13 21:53:45 +0800

    null

--- push.bat
+++ push.bat
@@ -1,7 +1,19 @@
 @echo off
+echo ************************************************
+echo PUSH RESOURCE BANK
+cd ../gitee.rb
 git add *
 git commit -m "null"
 git push origin master
+
+echo ************************************************
+echo PUSH FUNDMENTALS OF COMPUTERS
+cd ../gitee.fc
+git add *
+git commit -m "null"
+git push origin master
+
+echo ************************************************
 echo Press any key to exit
 pause
 exit
